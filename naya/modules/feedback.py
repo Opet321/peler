cb79a4d14c43c844978e6d09cdf6288da272aa2e
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-01-21 01:45:27 +0700

    update naya/modules/feedback.py

--- naya/modules/feedback.py
+++ naya/modules/feedback.py
@@ -1,70 +1,65 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from .utils import owner, owner_username, get_message_id, users, messages, bans
-import asyncio
-
-
-@Client.on_message(filters.command(['start']))
-async def start(client: Client, message: Message):
-    user_in_db = await users.find_one({'user': f'{message.from_user.id}'})
-    ban_list = await bans.find_one({'user': f'{message.from_user.id}'})
-    if not user_in_db:
-        await message.reply_text(f'<b>Hello,  {message.from_user.mention}!</b>', reply_to_message_id=message.message_id)
-        user = {'user': f'{message.from_user.id}'}
-        await users.insert_one(user)
-        await client.send_message(message.chat.id, f"<b>Send me your message and I'll forward it to {owner_username}.</b>")
-    else:
-        if not ban_list:
-            await message.reply_text(f"<b>Send me your message and I'll forward it to {owner_username}.</b>", reply_to_message_id=message.message_id)
-        else:
-            await message.delete()
-
-
-@Client.on_message(filters.chat(int(owner)))
-async def admin_messages(client: Client, message: Message):
-    last_msg = [_ async for _ in messages.find()][-1]
-    if message.reply_to_message:
-        if int(message.reply_to_message.from_user.id) != int(owner):
-            message_id = await get_message_id(message_id=message.reply_to_message.message_id)
-            await message.copy(int(message_id['user']), reply_to_message_id=int(message_id['message_id']))
-            message = await message.reply_text(f"<b>Your message was delivered to {(message_id['user'])}</b>", reply_to_message_id=message.message_id)
-            if int(last_msg['user']) != int(message_id['user']):
-                message_data = {'message_id_forward': f"{message_id['message_id_forward']}",
-                                'message_id': f"{message_id['message_id']}",
-                                'user': f"{message_id['user']}"}
-                await messages.insert_one(message_data)
-            await asyncio.sleep(2)
-            await message.delete()
-        else:
-            message_id = await get_message_id(message_id=last_msg['message_id_forward'])
-            await message.copy(int(message_id['user']))
-            message = await message.reply_text(f"<b>Your message was delivered to {(message_id['user'])}</b>", reply_to_message_id=message.message_id)
-            await asyncio.sleep(2)
-            await message.delete()
-
-    else:
-        message_id = await get_message_id(message_id=last_msg['message_id_forward'])
-        await message.copy(int(message_id['user']))
-        message = await message.reply_text(f"<b>Your message was delivered to {(message_id['user'])}</b>", reply_to_message_id=message.message_id)
-        await asyncio.sleep(2)
+from asyncio import sleep  
+from time import sleep 
+from pyrogram import Client, filters 
+from pyrogram.types import Message  
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli 
+from naya.config import MONGO_URL, OWNER 
+from . import * 
+ 
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL) 
+users = db.users  
+mongo = MongoCli(MONGO_URL) 
+messages = db.messages 
+ 
+ 
+async def _message_id(message_id): 
+ message_id = await messages.find_one({"forward_id": f"{message_id}"}) 
+ return message_id 
+ 
+ 
+@app.on_message(filters.chat(int(OWNER))) 
+async def _owner(client: Client, message: Message): 
+    last_msg = None  # Memberikan nilai awal untuk last_msg 
+    async for msg in messages.find(): 
+        last_msg = msg 
+ 
+    if message.reply_to_message: 
+        message_id = await _message_id(message.reply_to_message.id) 
+        if message_id: 
+            await message.copy(int(message_id['user_id']), reply_to_message_id=int(message_id['message_id'])) 
+            await message.reply_text(f"<b>Pesan Anda telah terkirim ke {(message_id['user_id'])}</b>", reply_to_message_id=message.id, disable_notification=True) 
+            if last_msg and int(last_msg['user_id']) != int(message_id['user_id']): 
+                message_data = { 
+                    "forward_id": f"{message_id['forward_id']}", 
+                    "message_id": f"{message_id['message_id']}", 
+                    "user_id": f"{message_id['user_id']}" 
+                } 
+                await messages.insert_one(message_data) 
+            await asyncio.sleep(5) 
+            await message.delete() 
+    else: 
+        if last_msg: 
+            message_id = await _message_id(last_msg['forward_id']) 
+            if message_id: 
+                await message.copy(int(message_id['user_id'])) 
+                await message.reply_text(f"<b>Pesan Anda telah terkirim ke {(message_id['user_id'])}</b>", reply_to_message_id=message.id, disable_notification=True) 
+                await asyncio.sleep(5) 
+                await message.delete() 
+        else: 
+            await message.reply_text("List is empty, cannot retrieve last message.") 
+ 
+ 
+@app.on_message(filters.all & filters.private & ~filters.me) 
+async def _user(client: Client, message: Message): 
+    user_db = await users.find_one({"user_id": f"{message.from_user.id}"}) 
+    if not user_db: 
+        await message.reply_text(f"<b>You are not in the database, enter /start to use the bot!</b>", reply_to_message_id=message.id) 
+    else: 
+        forwarded_message = await message.forward(OWNER) 
+        message_data = {"forward_id": f"{forwarded_message.id}", 
+                        "message_id": f"{message.id}", 
+                        "user_id": f"{message.from_user.id}"} 
+        await messages.insert_one(message_data) 
+        message = await message.reply_text(f"", reply_to_message_id=message.id, disable_notification=True) 
+        await asyncio.sleep(3) 
         await message.delete()
-
-
-@Client.on_message(filters.all & filters.private & ~filters.me)
-async def all_messages(client: Client, message: Message):
-    user_in_db = await users.find_one({'user': f'{message.from_user.id}'})
-    ban_list = await bans.find_one({'user': f'{message.from_user.id}'})
-    if not user_in_db:
-        await message.reply_text(f"<b>You are not in the database, enter /start to use the bot!</b>", reply_to_message_id=message.message_id)
-    else:
-        if not ban_list:
-            forwarded_message = await message.forward(owner)
-            message_data = {'message_id_forward': f'{forwarded_message.message_id}',
-                            'message_id': f'{message.message_id}',
-                            'user': f'{message.from_user.id}'}
-            await messages.insert_one(message_data)
-            message = await message.reply_text(f"<b>Your message was delivered to {owner_username}</b>", reply_to_message_id=message.message_id)
-            await asyncio.sleep(2)
-            await message.delete()
-        else:
-            await message.delete()
