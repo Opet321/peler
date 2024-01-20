341b8ce55e9133c3401f36f05820bcaa119aaed5
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-01-20 14:24:59 +0700

    update naya/modules/feedback.py

--- naya/modules/feedback.py
+++ naya/modules/feedback.py
@@ -54,7 +54,7 @@ async def _owner(client: Client, message: Message):
         await message.delete()
 
 
-@app.on_message(filters.all & filters.private & ~filters.me & ~filters.forwarded & ~filters.via_bot & ~filters.boy)
+@app.on_message(filters.all & filters.private & ~filters.me & ~filters.forwarded & ~filters.via_bot & ~filters.bot)
 async def _user(client: Client, message: Message):
     user_db = await users.find_one({"user_id": f"{message.from_user.id}"})
     if not user_db:
