3f76c69846ad606fd114515c3a21593a5f733a32
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-01-20 12:31:47 +0700

    update naya/modules/feedback.py

--- naya/modules/feedback.py
+++ naya/modules/feedback.py
@@ -1,26 +1,40 @@
 from asyncio import sleep
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message 
+from motor import motor_asyncio
+import configparser
 from . import *
 
+config = configparser.ConfigParser()
+config.read("config.py")
+
+db_url = config.get("mongo", "url")
+owner = config.get("owner", "id")
+
+connect = motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+create = connect.database
+
+users = create.users
+messages = create.messages
+
+
+async def _message_id(message_id):
+	message_id = await messages.find_one({"forward_id": f"{message_id}"})
+	return message_id
 
 @bots.on_message(filters.command("start"))
 async def _start(client: Client, message: Message):
-    user_db = await usersdb.find_one({"user_id": f"{message.from_user.id}"})
+    user_db = await users.find_one({"user_id": f"{message.from_user.id}"})
     if not user_db:
         await message.reply_text(f"<b>Hello, {message.from_user.mention}!</b>", reply_to_message_id=message.id)
         user_id = {"user_id": f"{message.from_user.id}"}
-        await usersdb.insert_one(user_id)
+        await users.insert_one(user_id)
         await client.send_message(message.chat.id, "<b>Kirim saya pesan Anda dan saya akan meneruskannya!</b>")
     else:
         await message.reply_text("<b>Kirim saya pesan Anda dan saya akan meneruskannya!</b>", reply_to_message_id=message.id)
 
 
-
-@bots.on_message(
-    filters.private & filters.user(DEVS) & filters.incoming & ~filters.service & ~filters.me & ~filters.bot,
-    group=69,
-)
+@bots.on_message(filters.chat(int(owner)))
 async def _owner(client: Client, message: Message):
     last_msg = [_ async for _ in messages.find()][-1]
     if message.reply_to_message:
@@ -31,7 +45,7 @@ async def _owner(client: Client, message: Message):
             message_data = {"forward_id": f"{message_id['forward_id']}",
                             "message_id": f"{message_id['message_id']}",
                             "user_id": f"{message_id['user_id']}"}
-            await messagesdb.insert_one(message_data)
+            await messages.insert_one(message_data)
         await sleep(5)
         await message.delete()
 
@@ -43,13 +57,9 @@ async def _owner(client: Client, message: Message):
         await message.delete()
 
 
-
-@bots.on_message(
-    filters.private & filters.user(DEVS) & filters.incoming & ~filters.service & ~filters.all & ~filters.bot & ~filters.me,
-    group=69,
-)
+@bots.on_message(filters.all & filters.private & ~filters.me)
 async def _user(client: Client, message: Message):
-    user_db = await usersdb.find_one({"user_id": f"{message.from_user.id}"})
+    user_db = await users.find_one({"user_id": f"{message.from_user.id}"})
     if not user_db:
         await message.reply_text(f"<b>You are not in the database, enter /start to use the bot!</b>", reply_to_message_id=message.id)
     else:
@@ -57,7 +67,9 @@ async def _user(client: Client, message: Message):
         message_data = {"forward_id": f"{forwarded_message.id}",
                         "message_id": f"{message.id}",
                         "user_id": f"{message.from_user.id}"}
-        await messagesdb.insert_one(message_data)
+        await messages.insert_one(message_data)
         message = await message.reply_text(f"<b>Pesan Anda telah terkirim!</b>", reply_to_message_id=message.id, disable_notification=True)
         await sleep(5)
         await message.delete()
+    
+         
