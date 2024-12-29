54fb8e0a9e01d2cca49e6d741cc78827043db8b0
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-12-29 09:24:46 +0700

    update naya/modules/feedback.py

--- naya/modules/feedback.py
+++ naya/modules/feedback.py
@@ -77,17 +77,28 @@ async def _owner(client: Client, message: Message):
             await message.reply_text("List is empty, cannot retrieve last message.")
  
  
-@app.on_message(filters.all & filters.private & ~filters.me) 
-async def _user(client: Client, message: Message): 
-    user_db = await users.find_one({"user_id": f"{message.from_user.id}"}) 
-    if not user_db: 
-        await message.reply_text(f"<b>You are not in the database, enter /start to use the bot!</b>", reply_to_message_id=message.id) 
-    else: 
-        forwarded_message = await forwarded_message.react("👍")(OWNER) 
-        message_data = {"forward_id": f"{forwarded_message.id}", 
-                        "message_id": f"{message.id}", 
-                        "user_id": f"{message.from_user.id}"} 
-        await messages.insert_one(message_data) 
-        message = await message.reply_text(f"Pesan Anda Telah Terkirim", reply_to_message_id=message.id, disable_notification=True) 
-        await asyncio.sleep(3) 
-        await message.delete()
+
+@app.on_message(filters.private & ~filters.me)
+async def _user(client: Client, message: Message):
+    user_db = await users.find_one({"user_id": f"{message.from_user.id}"})
+    if not user_db:
+        await message.reply_text(
+            f"You are not in the database, enter /start to use the bot!",
+            reply_to_message_id=message.id,
+        )
+    else:
+        forwarded_message = await message.forward(OWNER)
+        await forwarded_message.react("👍")
+        message_data = {
+            "forward_id": f"{forwarded_message.id}",
+            "message_id": f"{message.id}",
+            "user_id": f"{message.from_user.id}",
+        }
+        await messages.insert_one(message_data)
+        reply = await message.reply_text(
+            f"Pesan Anda Telah Terkirim",
+            reply_to_message_id=message.id,
+            disable_notification=True,
+        )
+        await asyncio.sleep(3)
+        await reply.delete()
