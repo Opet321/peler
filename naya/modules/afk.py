6f5b9b6c6d280936f495cbd6c2f602605a640425
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-12-29 19:21:43 +0700

    update naya/modules/afk.py

--- naya/modules/afk.py
+++ naya/modules/afk.py
@@ -101,7 +101,7 @@ async def _(client, message):
     await afk_handler.set_afk()
 
 
-@bots(True)
+@bots.AFK(True)
 async def _(client, message):
     afk_handler = AwayFromKeyboard(client, message)
     await afk_handler.get_afk()
