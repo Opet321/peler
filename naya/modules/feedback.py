88f2a545a5c0478e75a0be2e14f195196349508f
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-12-29 10:32:59 +0700

    update naya/modules/feedback.py

--- naya/modules/feedback.py
+++ naya/modules/feedback.py
@@ -1,8 +1,7 @@
 from asyncio import sleep  
 from time import sleep 
 from pyrogram import Client, filters 
-from pyrogram.types import Message  
-from pyrogram.enums import MessageReaction
+from pyrogram.types import Message, MessageReaction
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from naya.config import MONGO_URL, OWNER 
