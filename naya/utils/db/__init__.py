75968a49f4bd00afaa973e2c644150c0995360fa
Opet321 <90963402+Opet321@users.noreply.github.com>
2024-12-30 12:22:23 +0700

    update naya/utils/db/__init__.py

--- naya/utils/db/__init__.py
+++ naya/utils/db/__init__.py
@@ -39,7 +39,8 @@ blchatdb = db.blchat
 pmdb = db.pmpermit
 afkdb = db.afk
 resell = db.seles
-expdb = db.expired
+expdb = db.expired 
+varsdb = db.kintil
 vardb = db.variable
 sudoersdb = db.sudoers
 
@@ -151,7 +152,7 @@ async def babi(bot):
 
 async def remove_vars(user_id, vars_name, query="vars"):
     remove_data = {"$unset": {f"{query}.{vars_name}": ""}}
-    await vardb.update_one({"_id": user_id}, remove_data)
+    await varsdb.update_one({"_id": user_id}, remove_data)
 
 async def set_var(user_id, var, value):
     vari = await vardb.find_one({"user_id": user_id, "var": var})
