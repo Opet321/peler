079d151cda5a2f0b8d1362d1f746e36c44e5f520
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-06 19:58:22 +0700

    update naya/modules/pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -48,39 +48,31 @@ async def is_antipm_(f, client, message):
 
 is_antipm = filters.create(func=is_antipm_, name="is_antipm_")
 
-@bots.on_message(filters.command(["autopurge"], cmd) & filters.me)
+@app.on_message(filters.command(["autopurge"], cmd) & filters.me)
 async def set_antipm(client, message):
     try:
+        if len(message.command) < 2:
+            await message.reply("Please specify 'on' or 'off'.", quote=True)
+            return
+        
         if message.command[1] == "on":
             user_id = client.me.id
-            anukus = datetime.now()
             await go_antipm(user_id)
-            await message.reply(
-                f"<b>Anti-PM activated!!</b>",
-                quote=True
-                )
+            await message.reply("<b>Anti-PM activated!!</b>", quote=True)
         elif message.command[1] == "off":
             user_id = client.me.id
             await no_antipmk(user_id)
-            await message.reply(
-                f"<b>Anti-PM deactived!!</b>",
-                quote=True
-                )
+            await message.reply("<b>Anti-PM deactivated!!</b>", quote=True)
         else:
             kontols = await check_antipm(client.me.id)
-            try:
-                kurukuru = kontols["antipm"]
-            except:
-                kurukuru = "False"
+            kurukuru = kontols.get("antipm", "False")
             await message.reply(f"<b>Anti-PM status:</b> <code>{kurukuru}</code>\n<b>To Activate use</b> <code>antipm on/off</code>", quote=True)
-    except:
+    except Exception as e:
+        print(f"Error in set_antipm: {e}")
         kontols = await check_antipm(client.me.id)
-        try:
-            kurukuru = kontols["antipm"]
-        except:
-            kurukuru = "False"
+        kurukuru = kontols.get("antipm", "False")
         await message.reply(f"<b>Anti-PM status:</b> <code>{kurukuru}</code>\n<b>To Activate use</b> <code>antipm on/off</code>", quote=True)
-        
+
 
 @app.on_inline_query(
     ~filters.me
