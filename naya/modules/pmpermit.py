164eec507fa8ddf48b0aa1f4ef162b72602f4271
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 12:10:05 +0700

    update naya/modules/pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -75,43 +75,51 @@ async def set_antipm(client, message):
         await message.reply(f"<b>Anti-PM status:</b> <code>{kurukuru}</code>\n<b>To Activate use</b> <code>antipm on/off</code>", quote=True)
 
 
-
-
-
 @bots.on_message(
-    ~filters.me & ~filters.bot & filters.private & is_antipm
+   ~filters.me & ~filters.bot & filters
+     .private & is_antipm 
 )
-async def handle_antipm(client: Client, message: Message) -> None:
+async def handle_antipm(client: Client,
+  message: Message) -> None: 
     if message.from_user.is_contact is True:
-        return
+        return 
     if message.from_user.is_support is True:
+        return 
+    if message.from_user.id == OWNER: 
         return
-    if message.from_user.id == OWNER:
-        return
+      
+    result = await client 
+      .get_inline_bot_result("eyecosbot", query="pmpermit") 
+    await client.send_inline_bot_result
+      (message.chat.id, result.query_id,
+      result.results[0].id) 
+      
+    peer_id = await client.resolve_peer 
+      (message.chat.id) 
+    await client.invoke(DeleteHistory(peer=peer_id, max_id=0, revoke=True)
 
-    result = await client.get_inline_bot_result("@eyecosbot", query="pmpermit")
-    await client.send_inline_bot_result(message.chat.id, result.query_id, result.results[0].id)
-
-    peer_id = await client.resolve_peer(message.chat.id)
-    await client.invoke(DeleteHistory(peer=peer_id, max_id=0, revoke=True))
 
 
-# handler buat bot
-@app.on_inline_query()
-async def handle_inline(client: Client, inline: InlineQuery) -> None:
-    inline_query = inline.query
-    if inline_query.strip().lower().split()[0] == "pmpermit":
+
+@app.on_inline_query() 
+async def handle_inline(client: Client, inline: InlineQuery) -> None: 
+    inline_query = inline.query 
+    if inline_query.strip().lower().split()[0] == "pmpermit": 
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Teks button", callback_data="callback")]]
-        )
-        await inline.answer(
+            [[InlineKeyboardButton="Peler", callback_data="callback")]] 
+        ) 
+        await inline.answer( 
             results=[InlineQueryResultArticle(
-                id=str(uuid4()),
+                id=str(uuid4()), 
                 title="Pmpermit",
-                input_message_content=InputTextMessageContent("isi disini pesan inline via bot nya"),
-                reply_markup=keyboard
+                input_message_content 
+                  =InputTextMessageContent("gak usah pm"), 
+                reply_markup=keyboard 
             )]
         )
+
+
+        
 
 
 @bots.on_message(filters.command(["pmpermit", "antipm"], cmd) & filters.me)
