2c1c6864a2dc390114a47982fb7923e31f260bfb
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 14:23:09 +0700

    update naya/modules/pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -90,8 +90,8 @@ async def handle_antipm(client: Client,
       
     results = await client.get_inline_bot_results("@eyecosbot",  query="pmpermit") 
     await client.send_inline_bot_result(message.chat.id, results.query_id, results.results[0].id)
-      
-    peer_id = await client.resolve_peer(message.chat.id) 
+    peer_id = await client.resolve_peer(message.chat.id)  
+    await sleep (4)
     await client.invoke(DeleteHistory(peer=peer_id, max_id=0, revoke=True))
 
 
@@ -108,7 +108,7 @@ async def handle_inline(client: Client, inline: InlineQuery) -> None:
                 id=str(uuid4()), 
                 title="Pmpermit",
                 input_message_content 
-                  =InputTextMessageContent("peler"), 
+                  =InputTextMessageContent("<b><blockquote>Maaf saya tidak bisa menerima PM, Silahkan hubungi saya melalui bot"), 
                 reply_markup=keyboard 
             )]
         )
