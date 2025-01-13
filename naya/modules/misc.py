601f5323c3d0ec1c60661a3d82a28602f3142f31
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-13 15:15:47 +0700

    update naya/modules/misc.py

--- naya/modules/misc.py
+++ naya/modules/misc.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 )
 
 from . import *
-from config import FORUM_CHAT_ID
+from naya.config import *
 
 
 @app.on_message(filters.chat(FORUM_CHAT_ID) & filters.command("del"))
