32aefd92af6f29b273945a1a6b04f4e116358b0f
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-14 10:49:46 +0700

    update naya/modules/private.py

--- naya/modules/private.py
+++ naya/modules/private.py
@@ -10,7 +10,7 @@ from . import *
 from naya.config import *
 
 
-@bots.on_raw_update(group=1)
+@Client.on_raw_update(group=1)
 async def event_handler(client: Client, event: base.Update, _: User, __: Chat) -> None:
     if isinstance(event, types.UpdateBotStopped):
         user_id: int = event.user_id
@@ -23,7 +23,7 @@ async def event_handler(client: Client, event: base.Update, _: User, __: Chat) -
         await client.edit_forum_topic(FORUM_CHAT_ID, topic_id, title=topic_title)
 
 
-@bots.on_message(filters.private & ~filters.me)
+@Client.on_message(filters.private & ~filters.me)
 async def private_handler(client: Client, message: Message) -> None:
     if text := message.text:
         if text.startswith("/start"):
