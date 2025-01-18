3d60ecdb760393fe580d328b9c6fb90232cb3b47
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-18 10:51:37 +0700

    update naya/config.py

--- naya/config.py
+++ naya/config.py
@@ -6,10 +6,10 @@ load_dotenv(".env")
 
 
 API_ID = int(getenv("API_ID", "16712780"))
-OWNER = int(getenv("OWNER", "7183040046"))
+OWNER = int(getenv("OWNER", "6190309715"))
 API_HASH = getenv("API_HASH", "7941450f5313966647b6d6fde5f933dc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://apem:apem@cluster0.iraog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-BOT_TOKEN = getenv("BOT_TOKEN", "7647918583:AAFRli68VM40AJJk_7-L4XZ2m5NFGDbSzU0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7714141077:AAGLAjDfwtpxxBma_RPrO-zkxNDvS-AghVY")
 OPENAI_API = getenv("OPENAI_API", "") 
 FORUM_CHAT_ID = getenv("FORUM_CHAT_ID", "-1002314627378")
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
