e2157b6a88eb606a2a025c1a6392efc209475fbb
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-19 08:58:27 +0700

    update naya/modules/private.py

--- naya/modules/private.py
+++ naya/modules/private.py
@@ -81,7 +81,7 @@ async def _owner(client: Client, message: Message):
  
 @app.on_message(filters.all & filters.private & ~filters.me)
 async def _user(client: Client, message: Message): 
-    await message.react(emoji="❤️‍🔥")
+    await message.react(emoji="👀")
     user_db = await users.find_one({"user_id": f"{message.from_user.id}"})
     if not user_db:
         await message.reply_text(f"<b>You are not in the database, enter /start to use the bot!</b>", reply_to_message_id=message.id)
