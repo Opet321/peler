c7abc76a88deb06b1e6df5e5a6488e40207e3785
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-24 12:51:07 +0700

    update naya/modules/private.py

--- naya/modules/private.py
+++ naya/modules/private.py
@@ -42,7 +42,7 @@ async def _owner(client: Client, message: Message):
         message_id = await _message_id(message.reply_to_message.id)
         if message_id:
             sent_message = await message.copy(int(message_id['user_id']), reply_to_message_id=int(message_id['message_id']))
-            await message.reply_text(f"<b><blockquote>terkirim ke {message_id['user_id']}</b></blockquote>",  reply_to_message_id=message.id, disable_notification=True)
+            await message.reply_text(f"<b><blockquote>terkirim ke {message_id['user_id']}</b></blockquote>",  reply_parameters=message.id, disable_notification=True)
             
             await asyncio.sleep(3)
             
@@ -66,7 +66,7 @@ async def _owner(client: Client, message: Message):
             if message_id:
                 sent_message = await message.copy(int(message_id['user_id']))
                 
-                reply_message = await message.reply_text(f"<b><blockquote>terkirim ke {message_id['user_id']}</b></blockquote>", reply_to_message_id=message.id, disable_notification=True)
+                reply_message = await message.reply_text(f"<b><blockquote>terkirim ke {message_id['user_id']}</b></blockquote>", reply_parameters=message.id, disable_notification=True)
                 
                 await asyncio.sleep(3)
                 try:
