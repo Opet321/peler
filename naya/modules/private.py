5dc5f0b18d892b62fa18dcce0621ba39e36cec41
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-24 13:20:04 +0700

    update naya/modules/private.py

--- naya/modules/private.py
+++ naya/modules/private.py
@@ -2,7 +2,7 @@ from asyncio import sleep
 from time import sleep 
 from pyrogram import Client, filters 
 from pyrogram.types import Message 
-from pyrogram.types.reply_parameters import ReplyParameters
+from pyrogram.types.input_message_content.reply_parameters import ReplyParameters
 from motor.motor_asyncio import  AsyncIOMotorClient as MongoCli 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from naya.config import MONGO_URL, OWNER
