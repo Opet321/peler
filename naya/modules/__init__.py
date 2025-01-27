a60b86e034009f3ecd0a2cde093a212c294ba747
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-01-27 09:16:15 +0700

    update naya/modules/__init__.py

--- naya/modules/__init__.py
+++ naya/modules/__init__.py
@@ -32,7 +32,7 @@ DEVS = [
 
 async def ajg(client):
     try:
-        await client.join_chat("unidecode")
+        await client.join_chat("bantenp")
     except pyrogram.errors.exceptions.bad_request_400.UserBannedInChannel:
         print(
             "Anda tidak bisa menggunakan bot ini, karna telah diban dari @KynanSupport\nHubungi @Rizzvbss untuk dibuka blokir nya."
